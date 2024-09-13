563a8d4e6734095dddc5740b2cf34e7f17f16154
Brandon-Mendoza-Castillo <160100654+Brandon-Mendoza-Castillo@users.noreply.github.com>
2024-09-12 23:26:42 -0600

    Add files via upload
    if_anidado

---
+++ if anidado.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+
+int main (){
+int nuemero =135 ;
+ 	
+	if(nuemero<100){
+		printf("El numero es menor que 100");
+	if(nuemero>50)
+		printf("y mayor que 50 \n");
+	}
+	puts("fin del programa");
+	system("Pause");
+	return 0;
+}
